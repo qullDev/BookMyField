8179a63b81040598f58fa8d85a49c60488c2b6df
Rizqullah <mm.rizqullah@gmail.com>
2025-08-27 23:50:59 +0800

    refactor: reorganize main and db connection code for clarity and structure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/qullDev/BookMyField/internal/config"
+	"github.com/qullDev/BookMyField/internal/models"
+	"github.com/qullDev/BookMyField/internal/routes"
+)
+
+func main() {
+	if err := godotenv.Load("../../.env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r := gin.Default()
+
+	config.ConnectDatabse()
+
+	err := config.DB.AutoMigrate(&models.User{})
+	if err != nil {
+		log.Fatal("Error migrating database:", err.Error())
+		return
+	}
+
+	api := r.Group("/api/v1")
+	{
+		routes.AuthRoutes(api)
+		
+	}
+
+	r.Run(":8080")
+}
--- internal/config/db.go
+++ internal/config/db.go
@@ -1,1 +1,24 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabse() {
+	dsn := os.Getenv("DATABASE_URL")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err.Error())
+	}
+
+	DB = db
+	fmt.Println("✅Database connected")
+}
